9f414d58582170a7b97b0a50d33a125e62656e7b
Victoria <vitalina27@gmail.com>
2017-12-11 10:14:21 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,10 @@ def fizzbuzz(int)
   str = ""
   if int % 3 == 0
    str = "Fizz"
-   
+
    elsif int % 5 == 0
        str += "Buzz"
-    
+
     elsif int %5 != 0 && int % 3 != 0
       str = int
      end

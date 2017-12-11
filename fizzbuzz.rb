63b40bb5a842903b983fb841b613214f9da658cb
Victoria <vitalina27@gmail.com>
2017-12-11 10:34:14 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,18 +1,10 @@
-def fizzbuzz(int)
-  str = ""
-  if int % 3 == 0
-     if int%3 ==0 && int % 5 ==0
-        puts "FizzBuzz"
-    else str = "Fizz"
-    end
-   elsif int % 5 == 0
-     if int%3 ==0 && int % 5 ==0
-        puts "FizzBuzz"
-    else
-       str += "Buzz"
-     end
-    elsif int %5 != 0 && int % 3 != 0
-      str = ''
-     end
- puts str
+if int % 5 == 0 && int % 3 == 0
+    puts "Fizzbuzz"
+  elsif int % 3 == 0
+    puts "Fizz"
+  elsif int & 5 == 0
+    puts "Buzz"
+  else
+    puts int
+  end
 end

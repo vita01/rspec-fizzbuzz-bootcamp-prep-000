17a8de20b00da964747d880f989c95025eecbc83
Victoria <vitalina27@gmail.com>
2017-12-11 09:24:42 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
+    puts "Fizz" # Go fizz
   end
-elsif int % 5 ==0
-   "Buzz"
+elsif int % 5 == 0
+  puts  "Buzz"
  end
- else
-   "FizzBuzz"
+ else 
+  puts  "FizzBuzz"
  end
-
+  
 end
